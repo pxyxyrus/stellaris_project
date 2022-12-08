b1c7739ef62d6efc0aca7077c12033df8242d630
Daniel Kyutae Jung <pxyxyrus@gmail.com>
2022-12-08 03:17:09 -0800

    generate statistics

--- generate_statistics.py
+++ generate_statistics.py
@@ -6,6 +6,7 @@ from pathlib import Path
 # map_dir = "./greedy_vs_stupid_maps"
 # map_dir = "./slicing_vs_stupid_maps"
 
+
 def generate_csv(all_maps_dir: str):
     for j in [1, 201, 401, 601, 801]: 
         stats = []
@@ -32,7 +33,7 @@ def generate_csv(all_maps_dir: str):
                         line = f.readline()
                         agent1_name, agent1_num = line.split(',')
                         agent1_num = int(agent1_num)
-                        
+
                         line = f.readline()
                         agent2_name, agent2_num = line.split(',')
                         agent2_num = int(agent2_num)
@@ -56,7 +57,94 @@ def generate_csv(all_maps_dir: str):
 
 
 
+def gen_mean(all_maps_dir: str):
+    lines_to_write = []
+    for j in [200, 400, 600, 800, 1000]: 
+        agent1_mean = 0
+        agent2_mean = 0
+        agent1_name = ""
+        agent2_name = ""
+        cnt = 0
+        with open( "{}/statistics{}.csv".format(all_maps_dir, j), "r") as f:
+            print(f.readline())
+            agent1_name, agent2_name = f.readline().strip().split(",")
+
+            for line in f:
+                agent1_num, agent2_num = line.split(',')
+                agent1_mean += int(agent1_num)
+                agent2_mean += int(agent2_num)
+                cnt += 1
+
+
+        agent1_mean /= cnt
+        agent2_mean /= cnt
+
+        lines_to_write.append("{} stars statistic\n".format(j))
+        lines_to_write.append("{} mean : {}\n".format(agent1_name, agent1_mean))
+        lines_to_write.append("{} mean : {}\n\n".format(agent2_name, agent2_mean))
+        
+
+    with open("{}/mean.txt".format(all_maps_dir, j), "w") as f:
+        f.writelines(lines_to_write)
+
+
+
+def gen_stats_when_agent_won(all_maps_dir: str): 
+    lines_to_write = []
+    for j in [200, 400, 600, 800, 1000]:
+        agent1_name = ""
+        agent2_name = ""
+        agent1_win_agent1_mean = 0
+        agent1_win_agent2_mean = 0
+        agent2_win_agent1_mean = 0
+        agent2_win_agent2_mean = 0
+        agent1_win_count = 0
+        agent2_win_count = 0
+        with open( "{}/statistics{}.csv".format(all_maps_dir, j), "r") as f:
+            print(f.readline())
+            agent1_name, agent2_name = f.readline().strip().split(",")
+            
+            for line in f:                
+                agent1_num, agent2_num = line.split(',')
+                agent1_num = int(agent1_num)
+                agent2_num = int(agent2_num)
+                if agent1_num > agent2_num:
+                    agent1_win_agent1_mean += agent1_num
+                    agent1_win_agent2_mean += agent2_num
+                    agent1_win_count += 1
+                elif agent1_num < agent2_num:
+                    agent2_win_agent1_mean += agent1_num
+                    agent2_win_agent2_mean += agent2_num
+                    agent2_win_count += 1
+
+        agent1_win_agent1_mean /= agent1_win_count
+        agent1_win_agent2_mean /= agent1_win_count
+
+        agent2_win_agent1_mean /= agent2_win_count
+        agent2_win_agent2_mean /= agent2_win_count
+
+        lines_to_write.append("{} stars statistic\n".format(j))
+        lines_to_write.append("{} win statistics\n".format(agent1_name))
+        lines_to_write.append("{} games\n".format(agent1_win_count))
+        lines_to_write.append("{} mean : {}\n".format(agent1_name, agent1_win_agent1_mean))
+        lines_to_write.append("{} mean : {}\n".format(agent2_name, agent1_win_agent2_mean))
+        lines_to_write.append("diff: {}\n\n".format(agent1_win_agent1_mean - agent1_win_agent2_mean))
+
+        lines_to_write.append("{} win statistics\n".format(agent2_name))
+        lines_to_write.append("{} games\n".format(agent2_win_count))
+        lines_to_write.append("{} mean : {}\n".format(agent1_name, agent2_win_agent1_mean))
+        lines_to_write.append("{} mean : {}\n".format(agent2_name, agent2_win_agent2_mean))
+        lines_to_write.append("diff: {}\n\n".format(agent2_win_agent2_mean - agent2_win_agent1_mean))
+
+    with open("{}/win_stats.txt".format(all_maps_dir, j), "w") as f:
+        f.writelines(lines_to_write)
+
+
+
+
 if __name__ == "__main__":
     all_maps_dir = input("all_maps_dir? ")
-    generate_csv(all_maps_dir)
+    # generate_csv(all_maps_dir)
+    gen_mean(all_maps_dir)
+    gen_stats_when_agent_won(all_maps_dir)
     
